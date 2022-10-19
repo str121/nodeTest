2a231b857d6becce882526f7aac396ac945c815d
str121 <poilk2447@gmail.com>
2022-10-20 02:43:59 +0900

    중간 점검

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')   // 다운받은 express 모듈을 가져옴
 const app = express()           // 가져온 express function 사용
 const port = 5000                 // 사용할 port number, 임의적으로 사용 가능
 const mongoose = require('mongoose')    // mongoose 어플리케이션 사용
-
+const { User } = require("./models/User")
 const config = require('./config/key')
+const bodyParser = require('body-parser')
+
 
 
 app.get('/', (req, res) => {
@@ -15,7 +17,30 @@ app.listen(port, () => {
 })
 
 mongoose.connect(config.mongoURI, {
-	useNewUrlParser: true
+    useNewUrlParser: true
     // , userUnifiedTopology: true, userCresteIndex: true, useFindAndModify: true   // 에러
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
+
+
+//application/x-www-form-urlencoded, url encoded 형태로 오는 데이터 처리
+app.use(bodyParser.urlencoded({extended:true}));
+//aplication/json, json 형태로 오는 데이터 처리
+app.use(bodyParser.json());
+
+app.post('/regTest', (req, res) => {
+	const user = new User(req.body)
+	let now =  new Date();
+	res.send(now + "\n" + user);
+})
+
+app.post('/register', (req, res) => {
+	// 회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다
+	// body-parser가 
+	const user = new User(req.body)
+	// 몽고DB의 save메소드
+	user.save((err, userInfo) => {
+		if(err) return res.json({ success: false, err });
+		return res.status(200).json({ success: true });
+	})
+})
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
 	name: {
 		type: String,
@@ -25,5 +28,28 @@ const userSchema = mongoose.Schema({
 		type: Number,
 	},
 })
+
+userSchema.pre('save', function(next){
+	var user = this;   // == userSchema
+
+	if(user.isModified('password')){
+
+		// 비밀번호 암호화
+		bcrypt.genSalt(saltRounds, function(err, salt){
+			if(err) return next(err);
+			bcrypt.hash(user.password, salt, function(err, hash){
+				if(err) return next(err);
+				user.password = hash;
+				next();
+			})
+		})
+
+	} else {
+		next();
+	}
+
+}) 
+
+
 const User = mongoose.model('User', userSchema);        //('model의 이름', Schema), Schema를 Model로 감싸줌
 module.exports = { User };           // 다른 파일에서도 쓸 수 있게 export
