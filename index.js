23d60e5c85b5c0d5ea74227c8b1ccc76da9bdc96
str121 <ghkstkd41@naver.com>
2022-10-26 11:04:59 +0900

    로그인 및 토큰생성 작업 완료

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const express = require('express')   // 다운받은 express 모듈을 가져옴
 const app = express()           // 가져온 express function 사용
 const port = 5000                 // 사용할 port number, 임의적으로 사용 가능
-const mongoose = require('mongoose')    // mongoose 어플리케이션 사용
-const { User } = require("./models/User")
-const config = require('./config/key')
-const bodyParser = require('body-parser')
+
+const mongoose = require('mongoose');    // mongoose 어플리케이션 사용
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require("./models/User");
+const config = require('./config/key');
 
 
+//application/x-www-form-urlencoded -> url encoded 형태로 오는 데이터 처리
+app.use(bodyParser.urlencoded({extended:true}));
+//aplication/json -> json 형태로 오는 데이터 처리
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('2022-05-30,31일 생성 완료')
-})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
 
 mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true
+	useNewUrlParser: true
     // , userUnifiedTopology: true, userCresteIndex: true, useFindAndModify: true   // 에러
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
-//application/x-www-form-urlencoded, url encoded 형태로 오는 데이터 처리
-app.use(bodyParser.urlencoded({extended:true}));
-//aplication/json, json 형태로 오는 데이터 처리
-app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+	res.send('2022-05-30,31일 생성 완료')
+})
 
 app.post('/regTest', (req, res) => {
 	const user = new User(req.body)
@@ -35,12 +41,36 @@ app.post('/regTest', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-	// 회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다
+	// 회원가입 할 때 필요한 정보들을 (client에서 가져오면) 그것들을 DB에 넣어준다
 	// body-parser가 
 	const user = new User(req.body)
 	// 몽고DB의 save메소드
 	user.save((err, userInfo) => {
 		if(err) return res.json({ success: false, err });
-		return res.status(200).json({ success: true });
+		return res.status(200).json({ success: true, userInfo });
+	})
+})
+
+app.post('/test/login', (req, res) => {
+	// 요청된 이메일을 데이터베이스에 있는지 찾는다.
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if(!user){
+			return res.json({
+				loginSuccess: false,
+				message: "제공된 이메일에 해당하는 유저가 없습니다"
+			})
+		}
+		// 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if(!isMatch)
+				return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+			// 비밀번호까지 맞다면 토큰 생성
+			user.generateToken((err, user) => {
+				if(err) return res.status(400).send(err);
+				// 토큰을 저장 (쿠키/로컬스토리지,... etc)
+				// 쿠키에 토큰 저장
+				res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id })
+			})
+		})
 	})
 })
--- models/User.js
+++ models/User.js
@@ -1,32 +1,37 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+var jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
 	name: {
 		type: String,
-		maxlength: 50,
+		maxlength: 50
 	},
 	email: {
 		type: String,
 		trim: true,          // 중간의 space를 없애줌!
-		unique: 1,
+		unique: 1
+	},
+	password: {
+		type: String,
+		minlength: 4
 	},
 	lastname:{
 		type: String,
-		maxlength: 50,
+		maxlength: 50
 	},
 	role: {
 		type: Number,
-		default: 0,
+		default: 0
 	},
 	image: String,
 	token: {
-		type: String,
+		type: String
 	},      // 유효성검사 쓸ㄹ수잇오용
 	tokenExp: {
-		type: Number,
-	},
+		type: Number
+	}
 })
 
 userSchema.pre('save', function(next){
@@ -43,13 +48,35 @@ userSchema.pre('save', function(next){
 				next();
 			})
 		})
-
 	} else {
 		next();
 	}
-
 }) 
 
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+	// 입력하는 비밀번호와 암호화된 비밀번호 비교 
+	// 암호화된 비밀번호를 복호화 할 순 없음, 다시 원 비밀번호를 암호화시켜서 비교 
+	bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+		if(err) return cb(err);
+			cb(null, isMatch);
+	})
+}
+
+//userSchema의 스키마(json필드)에 token과 tokenExp값 설정해주기
+// 입력값 -> token: { type: String }, tokenExp : { type: Number },
+userSchema.methods.generateToken = function(cb){
+	// jsonwebtoken을 이용해서 토큰 생성하기
+	var user = this;
+	var token = jwt.sign(user._id.toHexString(), 'secretToken')
+	
+	// user._id + 'secretToken' 으로 token을 만듬 
+	// token 해석시 'secretToken'을 넣으면 user._id가 나옴
+	user.token = token
+	user.save(function(err, user) {
+		if(err) return cb(err)
+		cb(null, user)
+	})
+}
 
 const User = mongoose.model('User', userSchema);        //('model의 이름', Schema), Schema를 Model로 감싸줌
 module.exports = { User };           // 다른 파일에서도 쓸 수 있게 export
